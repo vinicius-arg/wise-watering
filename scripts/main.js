ebff374e02ddafa4241d43fd636b16b106966ccc
Vinícius <j.vin7cius@gmail.com>
2025-10-08 15:09:06 +0000

    feat: Arquitetura da interface gr´áfica

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// Campos do documento
+const monitor = document.querySelector('#monitor-umidade');
+const variacao = document.querySelector('#variacao');
+const prevTempo = document.querySelector('#prev-tempo');
+const autoIrrigacao = document.querySelector('#auto-irrigacao');
+const qtdAgua = document.querySelector('#qtd-agua');
+
+const botaoIrrigar = document.querySelector('#irrigar');
+const botaoSalvar = document.querySelector('#salvar');
